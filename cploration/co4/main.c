a5f5d71c80d0cd929f6d869931fc319451f2e00c
Dylan De Forrest <deforred@oregonstate.edu>
2024-10-28 19:50:20 -0700

    Complete project 3 through ram 512

--- cploration/co4/main.c
+++ cploration/co4/main.c
@@ -1,5 +1,5 @@
 /****************************************
- * C-ploration 3 for CS 271
+ * C-ploration 4 for CS 271
  * 
  * [NAME] Dylan De Forrest
  * [TERM] FALL 2024
@@ -8,4 +8,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "parser.h"
 #define MAX_LINE_LENGTH  200
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
+
+    parse(file);
+    
+ 
+    fclose(file);
+
+    return EXIT_SUCCESS;
+}
